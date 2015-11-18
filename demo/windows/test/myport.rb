c171b27e564e64902526c6f903cdf53fa890f0e0
Alex Pop <al3xpop@gmail.com>
2015-11-18 07:51:01 +0000

    need to start somewhere

---
+++ demo/windows/test/myport.rb
@@ -0,0 +1,11 @@
+control 'demo-test-1.0.0' do
+  impact 0.3
+  title '1.0.0 Not listening on port 80'
+  desc '
+    TCP Port 80 should not be listening.
+    We are enforcing encrypted web traffic over port 443.
+  '
+  describe port(80) do
+    it { should_not be_listening }
+  end
+end
