5c1e2ae525535af02354195d9352046d71bce1ec
Alex Pop <al3xpop@gmail.com>
2015-11-30 15:43:44 +0000

    removed vmetadata

--- demo/windows/metadata.rb
+++ demo/windows/metadata.rb
@@ -6,5 +6,5 @@ copyright_email 'contact@example.com'
 license 'Proprietary, All rights reserved'
 summary 'Verify that Windows nodes are configured securely'
 description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.1'
+version '1.0.3'
 supports 'windows'
--- demo/windows/test/ports.rb
+++ demo/windows/test/ports.rb
@@ -1,6 +1,6 @@
-control 'demo-test-1.0.0' do
+control 'demo-1.0.0' do
   impact 0.3
-  title '1.0.0 Not listening on port 80'
+  title '1.0.0 - Not listening on port 80'
   desc '
     TCP Port 80 should not be listening.
     We are enforcing encrypted web traffic over port 443.
